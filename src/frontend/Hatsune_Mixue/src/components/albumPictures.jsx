09b8ce98bebd1fd25dcc9287189032dceda0f043
Cola1000 <143616767+Cola1000@users.noreply.github.com>
2024-12-04 15:45:47 +0700

    ganti icon website

---
+++ src/frontend/Hatsune_Mixue/src/components/albumPictures.jsx
@@ -0,0 +1,27 @@
+const albumPictures = [
+  //ngeliatnya dari /src/frontend/Hatsune_Mixue <-- here
+
+  {
+    id: 1,
+    title: 'Album One',
+    imageSrc: '/tmpPictures/The_Stellar_Moments_1 copy 2.jpg',
+    songs: [
+      { id: 1_1, title: 'Song A', file: 'path_to_song_a.mp3' },
+      { id: 1_2, title: 'Song B', file: 'path_to_song_b.mp3' },
+      // Add more songs
+    ],
+  },
+  {
+    id: 2,
+    title: 'Album Two',
+    imageSrc: '/tmpPictures/The_Stellar_Moments_1 copy 2.jpg',
+    songs: [
+      { id: 2_1, title: 'Song A', file: 'path_to_song_a.mp3' },
+      { id: 2_2, title: 'Song B', file: 'path_to_song_b.mp3' },
+      // Add more songs
+    ],
+  },
+  // Add more albums
+];
+
+export default albumPictures;
