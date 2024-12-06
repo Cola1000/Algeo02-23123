a5574f8b90e84d2e5b9e513b60523de0453370e5
Cola1000 <rhiobimoprakoso.s@gmail.com>
2024-12-06 11:08:48 +0700

    Added physics for further UX

--- src/frontend/Hatsune_Mixue/src/pages/Home3D.jsx
+++ src/frontend/Hatsune_Mixue/src/pages/Home3D.jsx
@@ -5,6 +5,7 @@ import Konbini from '../models/konbini(backup)';
 import MovementController from '../components/MovementController';
 import { useNavigate } from 'react-router-dom';
 import hdri from "../assets/moonless_golf_8k.exr"
+import { Physics } from '@react-three/cannon';
 
 const Home3D = () => {
   const cameraRef = useRef();
@@ -33,31 +34,32 @@ const Home3D = () => {
 
   return (
     <div style={{ width: '100vw', height: '100vh', position: 'absolute' }}>
-      <Canvas shadows>
-        <Suspense fallback={null}>
-          <Environment files={hdri} background={true} />
-          
-          <PerspectiveCamera ref={cameraRef} makeDefault position={[0, 2, 5]} rotation={[0, 180, 0]} />
-          <hemisphereLight intensity={0.5}/>
-          <ambientLight intensity={0} />
-          <directionalLight
-              position={[5, 10, 5]}
-              intensity={1.5}
-              castShadow
-              shadow-bias={-0.0015}
-              shadow-mapSize-width={1024}
-              shadow-mapSize-height={1024}
-              shadow-camera-far={50}
-              shadow-camera-left={-10}
-              shadow-camera-right={10}
-              shadow-camera-top={10}
-              shadow-camera-bottom={-10}
-            />
-          <Konbini position={[3, 0, 2]} scale={[1, 1, 1]} rotation={[0, 0, 0]} />
-          <MovementController cameraRef={cameraRef} />
-        </Suspense>
-      </Canvas>
-
+      <Physics>
+        <Canvas shadows>
+          <Suspense fallback={null}>
+            <Environment files={hdri} background={true} />
+            
+            <PerspectiveCamera ref={cameraRef} makeDefault position={[0, 2, 5]} rotation={[0, 180, 0]} />
+            <hemisphereLight intensity={0.5}/>
+            <ambientLight intensity={0} />
+            <directionalLight
+                position={[5, 10, 5]}
+                intensity={1.5}
+                castShadow
+                shadow-bias={-0.0015}
+                shadow-mapSize-width={1024}
+                shadow-mapSize-height={1024}
+                shadow-camera-far={50}
+                shadow-camera-left={-10}
+                shadow-camera-right={10}
+                shadow-camera-top={10}
+                shadow-camera-bottom={-10}
+              />
+            <Konbini position={[3, 0, 2]} scale={[1, 1, 1]} rotation={[0, 0, 0]} />
+            <MovementController cameraRef={cameraRef} />
+          </Suspense>
+        </Canvas>
+      </Physics>
       <Loader containerStyles={{ position: 'absolute', top: '0', left: '0' }} />
     </div>
   );
